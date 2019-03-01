fdbd5cd88b6e7c534bdb460f7b82258d639c0fab
Thanh Dao <thanhdao@nexty.io>
2019-03-01 18:22:55 +0700

    Config env

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,12 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
+
+require('dotenv').config()
+
 module.exports = {
    networks: {
 	development: {
-		//provider : () => new HDWalletProvider(process.env.PRIVATE_KEY,"http://45.77.22.149:8545"),
-		provider : () => new HDWalletProvider(process.env.PRIVATE_KEY,"http://13.228.68.50:8545"),
+		provider : () => new HDWalletProvider(process.env.PRIVATE_KEY,"http://45.77.22.149:8545"),
+		//provider : () => new HDWalletProvider(process.env.PRIVATE_KEY,"http://13.228.68.50:8545"),
 		network_id: "*"
 	},
 	production: {
